5ce7f2b688c48223cd6b2af1cf7b5085aa9e01e4
Peter Postma <peter.postma@kentaa.nl>
2021-06-24 14:50:52 +0200

    Spec for Kentaa::Api::Util.

---
+++ spec/kentaa/api/util_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Kentaa::Api::Util do
+  describe '.pluralize' do
+    it 'returns the pluralized text' do
+      expect(described_class.pluralize("donation")).to eq("donations")
+    end
+  end
+end
